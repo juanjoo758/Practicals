d60cc0cfe8d62b94f15119474a2bc856382415e1
juanjoo758 <juanotero1226@gmail.com>
2025-09-09 22:11:54 -0300

    actualizo archivos

--- 01_fundamentos/02_operacionesbasicas.py
+++ 01_fundamentos/02_operacionesbasicas.py
@@ -1,1 +1,12 @@
 x = input("Introduce un número: ")
+x = int(x)
+y = input("Introduce otro número: ")
+y = int(y)
+suma = x + y
+resta = x - y
+multiplicacion = x * y
+division = x / y
+print("La suma es:", suma)
+print("La resta es:", resta)
+print("La multiplicación es:", multiplicacion)
+print("La división es:", division)
